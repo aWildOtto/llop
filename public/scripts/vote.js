356643936fa85fbcfa36f104bbf8a5e9527c85d1
Ti Zhang <tizhang8835@gmail.com>
2017-06-22 22:20:48 +0000

    vote.ejs all set up with interactive jQuery, just needing css and backend database query

--- public/scripts/vote.js
+++ public/scripts/vote.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
+// database stand-in, delete later
   var question = {
     id: 123,
     content: 'Which fruit is the root of all evils and should be eliminated from the earth?'
   };
-
   var obj = {
     item1 : { id: 1,
               content : 'apple'
@@ -17,19 +17,27 @@ $(document).ready(function(){
             },
     item4 : { id: 'new',
               content: 'melon'
+            },
+    item5 : { id: 'donald',
+              content: 'pineapple'
+            },
+    item6 : { id: 'caitlin',
+            content: 'blackberries'
             }
   };
 
+// jQuery sortable UI for drag and drop
   $(function sortable() {
         $( "#sortable" ).sortable({
             placeholder: 'ui-sortable-placeholder'
         });
     });
 
+// Dynamically generate list of options, store id of choices in custom attribute
   function makeList(obj){
     var $div = $('<div>');
     $div.append(question.content).attr('questionId', question.id);
-    $('#vote_form').prepend($div);
+    $('.question').append($div);
     for (var item in obj) {
       var $li = $('<li>');
       $li.append(obj[item].content).addClass('ui-state-default').attr('choiceId', obj[item].id);
@@ -39,6 +47,19 @@ $(document).ready(function(){
 
   makeList(obj);
 
+//error handling for empty name + show poll div once name submitted
+  $('.submit_name').on('click', function(event) {
+    event.preventDefault();
+    var $nameLength = $('#vote_form input').val().length;
+    if($nameLength === 0) {
+      alert('Hey bud, your name can\'t be empty  (Ծ‸ Ծ)');
+    } else {
+      $('.vote_container').show();
+    }
+  })
+
+//retrieve choiceId stored in custom attribute, create array
+// with index of the item being the voted rank
   $('#vote_form').on('submit', function(event) {
     event.preventDefault();
     var $sortableList = $('.sortable');
@@ -50,4 +71,6 @@ $(document).ready(function(){
     }
     console.log(choiceIdsRanked);
   })
+
+
 });
