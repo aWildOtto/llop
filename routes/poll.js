8eb460e865977fa62af0757026b6a55f499daaf0
otto hu <ottohu101@gmail.com>
2017-06-22 04:10:16 +0000

    temp save

--- routes/poll.js
+++ routes/poll.js
@@ -15,26 +15,26 @@ module.exports = (knex) => {
 
   router.post('/', (req,res)=>{
     knex('polls').insert({question: req.body.question, 
-                        creator_name: req.body.creator_name,
-                        creator_email: req.body.creator_email,
-                        background_path: req.body.background_path,
-                        anonymous: req.body.anonymous
-                      }).asCallback((err, result)=>{
-                        if(err){
-                          console.log(err);
-                        }
-                        let i = 0;
-                        let desc;
-                        req.body.title.forEach(function(element) {
-                          desc = req.body.description[i];
-                          knex('choice').insert({title: element, description: desc}).asCallback((err,result)=>{
-                            if(err){
-                              console.log(err);
-                            }
-                          })
-                          i++;
-                        });  
-                      });
+                          creator_name: req.body.creator_name,
+                          creator_email: req.body.creator_email,
+                          background_path: req.body.background_path,
+                          anonymous: req.body.anonymous
+      }).asCallback((err, result)=>{
+        if(err){
+          console.log(err);
+        }
+        let i = 0;
+        let desc;
+        req.body.title.forEach(function(element) {
+          desc = req.body.description[i];
+          knex('choice').insert({title: element, description: desc}).asCallback((err,result)=>{
+            if(err){
+              console.log(err);
+            }
+          })
+          i++;
+        });
+      });
   });
   return router;
 }
