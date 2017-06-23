a85d8b6e01095702f486c7df474a11fdd35f66d7
caitlinquon <caitlin_quon@hotmail.com>
2017-06-23 19:28:14 +0000

    temp save

--- public/scripts/create_poll.js
+++ public/scripts/create_poll.js
@@ -2,18 +2,20 @@ $(document).ready(function () {
   var max_fields = 20; //maximum input boxes allowed
   var wrapper = $(".input_fields_wrap"); //Fields wrapper
   var add_button = $(".add_field_button"); //Add button ID
-  var x = 2; //initlal text box count
+  var x = 0; //initlal text box count
   $(add_button).click(function (e) { //on add input button click
     e.preventDefault();
     if (x < max_fields) { //max input box allowed
       x++; //text box increment
       $(wrapper).append(
         `<div>
-          <label>Option ${x} </label>
+          <span class="input-group-addon-primary>
+            <span class="text_label"><i class="glyphicon glyphicon-record"></i></span>
+          </span>
           <input class="option" type="text" name="title[]">
-          <input rows="3" name="description[]" placeholder = "description">
+          <input class="description" rows="3" name="description[]" placeholder = "description">
           <button class="remove_field btn-remove btn-circle btn-lg"><i class="glyphicon glyphicon-remove"></i></button>
-        </div>`); //add input box
+        </div>`); //add input box 
     }
   });
   $(wrapper).on("click", ".remove_field", function (e) { //user click on remove text
