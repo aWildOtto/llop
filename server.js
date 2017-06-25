e5ce6e9a570ef391d1b6decc6c5f92f5fca711b4
Ti Zhang <tizhang8835@gmail.com>
2017-06-25 19:43:19 +0000

    heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 "use strict";
 
 // require('dotenv').config();
-// require('dotenv').config({silent: true});
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+require('dotenv').config({silent: true});
+// if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const PORT        = process.env.PORT || 8080;
 const ENV         = process.env.ENV || "development";
