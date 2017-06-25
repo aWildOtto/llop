6a01a97376798acd825e0a9a0056fbe073febfed
Ti Zhang <tizhang8835@gmail.com>
2017-06-25 02:41:32 +0000

    heroku

--- knexfile.js
+++ knexfile.js
@@ -28,10 +28,10 @@ module.exports = {
     connection: 'postgres://mvjvhsltypwpeu:5d4181b29dd99843172609769cf5e2c673aca460f470b536cb858148b1524f0f@ec2-23-23-222-147.compute-1.amazonaws.com:5432/db8p8fm4m6ek4q?ssl=true',
     ssl: true,
     debug: true,
-    pool: {
-      min: 2,
-      max: 10
-    },
+    // pool: {
+    //   min: 2,
+    //   max: 10
+    // },
     migrations: {
       directory: './db/migrations',
       tableName: 'migrations'
